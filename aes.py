37962d4af012574e2853188960a47ae6b826be64
Antonio A Abello <antonio.augusto.abello@gmail.com>
2018-04-07 00:32:17 -0300

    add 8bit operations

--- aes.py
+++ aes.py
@@ -1,14 +1,83 @@
 def int_to_poly(n):
-    pass
+    pol = []
+    while (n > 0):
+        pol.append(n % 2)
+        n = n // 2
+    return pol
 
 def poly_to_int(p):
-    pass
+    res = 0
+    for i in range(0, len(p)):
+        res += pow(2, i) * p[i]
+    return res
 
+def diff_poly(p1, p2):
+    ans = []
+    if (len(p2) >= len(p1)):
+        for i in range(0, len(p1)):
+            ans.append(p1[i] - p2[i])
+        for j in range(0, len(p2) - len(p1)):
+            ans.append(-1 * p2[len(p1) + j])
+    else:
+        for i in range(0, len(p2)):
+            ans.append(p1[i] - p2[i])
+        for j in range(0, len(p1) - len(p2)):
+            ans.append(p1[len(p2) + j])
+    last = ans.pop()
+    while last == 0 and len(ans) > 0:
+        last = ans.pop()
+    ans.append(last)
+    return ans
+        
 def mult_poly(p1, p2):
-    pass
+    ans = [0] * (len(p1) + len(p2) - 1)
+    
+    for i in range(0, len(p1)):
+        for j in range(0, len(p2)):
+            ans[i + j] += p1[i] * p2[j]
+    
+    ans = list(map(lambda x: x % 2, ans))
+    while ans.pop() == 0:
+        pass
+    ans.append(1)
+    return ans            
 
-def poly_mod(p1, pmod):
-    pass
-
+#Assumes that len(p1) - 1 = last significative digit
+def mod_poly(p1, pmod): 
+    while (len(p1) >= len(pmod)):
+        diff = len(p1) - len(pmod)
+        paux = []
+        for i in range(0, diff):
+            paux.append(0)
+        paux.append(1)
+        paux = mult_poly(paux, pmod)
+        p1 = diff_poly(p1, paux)
+        p1 = list(map(lambda x: x % 2, p1))
+    return p1
+  
 def mult8bits(bit1, bit2):
-    pass
+    print("{} convertido para polinomio".format(hex(bit1)))
+    p1 = int_to_poly(bit1)
+    print(p1)
+    print("{} convertido para polinomio".format(hex(bit2)))
+    p2 = int_to_poly(bit2)
+    print(p2)
+    print("Multiplicacao de polinomios")
+    ans = mult_poly(p1, p2)
+    print(ans)
+    print("Modulo 0x11B")
+    ans = mod_poly(ans, int_to_poly(0x11B))
+    print(ans)
+    print("Converte em numero")
+    return poly_to_int(ans)
+
+#calcula inversa pmod
+def inversemodpmod(bit1, bitmod):
+    p1 = int_to_poly(bit1)
+    pmod = int_to_poly(bitmod)
+    paux = p1
+    while poly_to_int(paux) != 1:
+        plast = paux
+        paux = mult_poly(p1, paux)
+        paux = mod_poly(paux, pmod)
+    return plast
--- test_all.py
+++ test_all.py
@@ -1,28 +1,39 @@
 import aes
 
 ##
-
 def test_int_to_poly():
-    assert aes.int_to_poly(0x45) == [0, 1, 0, 0, 0, 1, 0, 1]
-    assert aes.int_to_poly(0x0A) == [0, 0, 0, 0, 1, 0, 1, 0]
-    assert aes.int_to_poly(0x94) == [1, 0, 0, 1, 0, 1, 0, 0]
+    assert aes.int_to_poly(0x45) == [1, 0, 1, 0, 0, 0, 1]
+    assert aes.int_to_poly(0x0A) == [0, 1, 0, 1]
+    assert aes.int_to_poly(0x94) == [0, 0, 1, 0, 1, 0, 0, 1]
 
 def test_poly_to_int():
-    assert aes.poly_to_int([0, 1, 0, 0, 0, 1, 0, 1]) == 0x45
-    assert aes.poly_to_int([0, 0, 0, 0, 1, 0, 1, 0]) == 0x0A
-    assert aes.poly_to_int([1, 0, 0, 1, 0, 1, 0, 0]) == 0x94
+    assert aes.poly_to_int(aes.int_to_poly(0x45)) == 0x45
+    assert aes.poly_to_int(aes.int_to_poly(0x0A)) == 0x0A
+    assert aes.poly_to_int(aes.int_to_poly(0x94)) == 0x94
+
+def test_diff_poly():
+    pola = [1]
+    polb = [1]
+    assert aes.diff_poly(pola, polb) == [0]
+    polc = [1, 1]
+    pold = [1]
+    assert aes.diff_poly(polc, pold) == [0, 1]
+    assert aes.diff_poly(pold, polc) == [0, -1]
+    pole = [0, 1, 0, 0, 0, 1, 0, 1, 0, 1]
+    polf = [0, 1, 1, 0, 1, 1, 0, 0, 0, 1]
+    assert aes.diff_poly(pole, polf) == [0, 0, -1, 0, -1, 0, 0, 1]
 
 def test_mult_poly():
-    pola = [0, 1, 0, 0, 0, 1, 0, 1]
-    polb = [0, 0, 0, 0, 1, 0, 1, 0]
-    polans = [1, 0, 1, 0, 1, 0, 0, 0, 1, 0]
+    pola = aes.int_to_poly(0x45)
+    polb = aes.int_to_poly(0x0A)
+    polans = [0, 1, 0, 0, 0, 1, 0, 1, 0, 1] 
     assert aes.mult_poly(pola, polb) == polans
 
-def test_poly_mod():
-    pola = [1, 0, 1, 0, 1, 0, 0, 0, 1, 0]
-    polmod = [1, 0, 0, 1, 0, 1, 0, 0]
-    polans = [1, 0, 0, 0, 1, 1, 0, 1, 1]
-    assert aes.poly_mod(pola, polmod) == polans
+def test_mod_poly():
+    pola = [0, 1, 0, 0, 0, 1, 0, 1, 0, 1] 
+    polmod = aes.int_to_poly(0x11B)
+    polans = aes.int_to_poly(0x94)
+    assert aes.mod_poly(pola, polmod) == polans
 
 def test_mult8bits():
     assert aes.mult8bits(0x45, 0x0A) == 0x94
